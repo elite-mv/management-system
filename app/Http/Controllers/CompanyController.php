8a7801efe92111f17732884a27991dd2c31878be
John <johnzunigacastillo@gmail.com>
2024-08-15 09:01:27 +0800

    add entity

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function  index()
+    {
+
+        $companies = Company::paginate(10);
+
+        return view('entity', [
+            'companies' => $companies,
+        ]);
+    }
+}
