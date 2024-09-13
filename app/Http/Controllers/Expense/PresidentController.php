7faaff64c81aafdf252f71f27cbed17eda83d80f
john <john.castillo@eliteacesinc.com>
2024-09-14 07:27:24 +0800

    add total

--- app/Http/Controllers/Expense/PresidentController.php
+++ app/Http/Controllers/Expense/PresidentController.php
@@ -6,9 +6,11 @@ use App\Enums\RequestApprovalStatus;
 use App\Enums\UserRole;
 use App\Helper\Helper;
 use App\Models\Expense\Request as ModelsRequest;
+use App\Models\Expense\RequestItem;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use function PHPUnit\TestFixture\func;
 
 class PresidentController extends Controller
 {
@@ -79,11 +81,66 @@ class PresidentController extends Controller
 
         $requests = $query->paginate($request->input('entries') ?? 20, ['*'], 'page', $request->input('page') ?? 1);
 
+        $total = self::getTotal($request);
+
         return view('expense.president-requests', [
             'requests' => $requests,
-            'total' => 0,
+            'total' => $total,
         ]);
     }
+
+    public function getTotal(Request $request)
+    {
+
+        return RequestItem::select([DB::raw('SUM(quantity * cost) as total_cost')])
+            ->whereHas('request', function ($query) use ($request) {
+
+                $query->with('company', function ($query) {
+                    $query->select(['id', 'name']);
+                });
+
+                $query->when($request->input('search'), function ($qb) use ($request) {
+                    $qb->where(function ($qb) use ($request) {
+                        $qb->where('id', Helper::rawID($request->input('search')));
+                        $qb->orWhere('request_by', 'LIKE', $request->input('search') . '%');
+                        $qb->orWhere('reference', 'LIKE', $request->input('search') . '%');
+                    });
+                });
+
+                $query->when($request->input('entity') && $request->input('entity') != 'ALL', function ($qb) use ($request) {
+                    $qb->where('company_id', $request->input('entity'));
+                });
+
+                $query->when($request->input('paymentStatus') && $request->input('paymentStatus') != 'ALL', function ($qb) use ($request) {
+                    $qb->where('status', $request->input('paymentStatus'));
+                });
+
+                $query->when($request->input('from'), function ($qb) use ($request) {
+                    $qb->whereDate('created_at', '>=', Carbon::createFromFormat('Y-m-d', $request->input('from'))->toDateString());
+                });
+
+                $query->when($request->input('to'), function ($qb) use ($request) {
+                    $qb->whereDate('created_at', '<=', Carbon::createFromFormat('Y-m-d', $request->input('to'))->toDateString());
+                });
+
+                $query->whereHas('approvals', function ($qb) use ($request) {
+
+                    $qb->when($request->input('status') && $request->input('status') != 'ALL', function ($q) use ($request) {
+
+                        $q->where('status', RequestApprovalStatus::valueOf($request->input('status')));
+
+                        $q->whereHas('role', function ($q) {
+                            $q->where('name', UserRole::PRESIDENT->value);
+                            $q->orWhere('name', UserRole::FINANCE->value);
+                        });
+                    });
+                });
+
+            })
+            ->value('total_cost');
+
+    }
+
 
     public function getRequests(Request $request)
     {
