b172a9460729ff1357af3df8e4bad8b29dc808f9
john <john.castillo@eliteacesinc.com>
2024-10-03 15:39:50 +0800

    add pre-wrap

--- resources/views/expense/partials/request-comments.blade.php
+++ resources/views/expense/partials/request-comments.blade.php
@@ -3,7 +3,7 @@
         <div class="ms-auto text-end card p-3 mb-2 w-75">
             <div class="small">
                 <p class="text-capitalize text-primary mb-0">{{$comment->user->name}}:</p>
-                <p class="mb-1">{{$comment->message}}</p>
+                <p class="mb-1" style="white-space: pre-wrap">{{$comment->message}}</p>
                 <p class="mb-0 text-end text-secondary">{{$comment->created_at->format('Y-m-d H:m')}}</p>
             </div>
         </div>
@@ -11,7 +11,7 @@
         <div class="card w-75 p-3 mb-2">
             <div class="small">
                 <p class="text-capitalize text-primary mb-0">{{$comment->user->name}}:</p>
-                <p class="mb-1">{{$comment->message}}</p>
+                <p class="mb-1" style="white-space: pre-wrap">{{$comment->message}}</p>
                 <p class="mb-0 text-end text-secondary">{{$comment->created_at->format('Y-m-d H:m')}}</p>
             </div>
         </div>
